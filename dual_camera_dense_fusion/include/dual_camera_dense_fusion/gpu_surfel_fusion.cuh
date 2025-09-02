43a4f012fdc7d6e47159a0c2e8bc18869e0b434e
Wellknownbuffoon <102240121@student.vgu.edu.vn>
2025-09-02 22:12:09 +0700

    Create gpu_surfel_fusion.cuh

---
+++ dual_camera_dense_fusion/include/dual_camera_dense_fusion/gpu_surfel_fusion.cuh
@@ -0,0 +1,4 @@
+#pragma once
+// GPU surfel fusion CUDA headers go here
+// Implement distance-adaptive surfels, curvature, and GPU merging
+// Placeholder: actual CUDA kernels implementation is needed
